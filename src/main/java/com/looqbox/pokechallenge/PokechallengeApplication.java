b959fc204720e71458b95edb5b0c5ed483a302f6
FelipeGuerrino <felipe_bg2005@hotmail.com>
2022-04-27 21:47:42 -0300

    Initial Commit

---
+++ src/main/java/com/looqbox/pokechallenge/PokechallengeApplication.java
@@ -0,0 +1,13 @@
+package com.looqbox.pokechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokechallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokechallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/looqbox/pokechallenge/PokechallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.looqbox.pokechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokechallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
